000a839f67721cd73f61f7aa69a5c4d07f0da209
Bruno Carvalho <bruno.carvalho@cooasgo.local>
2023-10-23 08:59:53 -0300

    create app

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,6 +11,7 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap"
             rel="stylesheet"
           />
+          <link rel="shortcut icon" href="/https://github.com/BRL4528/ig.news/assets/50926585/16d2866d-e5cc-4568-9465-a3b3131bcf0e" type="image/png" />
         </Head>
         <body>
           <Main />
